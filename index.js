f69e29ba8e3b2cfcb6a54feaef654d71085a0bd1
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-09-06 20:26:43 +0530

    v2.0.0

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var Stream = require('stream'),
     xml2js = require('xml2js'),
-    _ = require('lodash'),
+    isFunction = require('lodash.isfunction'),
+    isObject = require('lodash.isobject'),
+    assign = require("object-assign"),
     gutil = require('gulp-util');
- 
+
 var xmlEdit = function(transform, options){
 
-    if(!_.isFunction(transform)){
+    if(!isFunction(transform)){
         transform = function(data){
             return data;
         };
     }
-    
+
     var defaults = {
         parserOptions: {},
         builderOptions: {
@@ -21,30 +23,30 @@ var xmlEdit = function(transform, options){
         }
     }
 
-    var settings = _.assign(defaults,options);
+    var settings = assign(defaults, options);
 
     var stream = new Stream.Transform({ objectMode: true });
 
     stream._transform = function(file, unused, done){
-        
+
         var that = this;
 
         if (file.isNull()) {
             return done(null, file);
         }
- 
+
         if (file.isStream()) {
           return done(new gutil.PluginError('gulp-xml-edit', 'Streaming not supported'));
         }
 
-        
+
         var content = file.contents.toString('utf-8'),
             parser = new xml2js.Parser(settings.parserOptions),
             builder = new xml2js.Builder(settings.builderOptions);
 
         parser.parseString(content, function(err,data){
             var content = transform.call(null, data);
-            if(!_.isObject(content)){
+            if(!isObject(content)){
                 done(new gutil.PluginError('gulp-xml-edit', 'transformation does not returns an object'));
                 return;
             }
@@ -53,8 +55,8 @@ var xmlEdit = function(transform, options){
             return done(null, file);
         });
     };
-    
+
     return stream;
 }
- 
+
 module.exports = xmlEdit;
