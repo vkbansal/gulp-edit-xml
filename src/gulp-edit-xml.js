cb50483e5e813d9792c0f66813887671c1f6e86a
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2019-11-21 08:29:13 +0530

    Update packages and scripts (#3)
    * Update packages and scripts
    
    * remove if conditions  in workflows

---
+++ src/gulp-edit-xml.js
@@ -0,0 +1,60 @@
+const Stream = require('stream');
+const xml2js = require('xml2js');
+const isFunction = require('lodash.isfunction');
+const isObject = require('lodash.isobject');
+const assign = require('object-assign');
+const PluginError = require('plugin-error');
+
+const xmlEdit = function(transform, options) {
+  if (!isFunction(transform)) {
+    transform = function(data) {
+      return data;
+    };
+  }
+
+  const defaults = {
+    parserOptions: {},
+    builderOptions: {
+      headless: true,
+      renderOpts: {
+        pretty: false
+      }
+    }
+  };
+
+  const settings = assign(defaults, options);
+
+  const stream = new Stream.Transform({ objectMode: true });
+
+  stream._transform = function(file, unused, done) {
+    if (file.isNull()) {
+      return done(null, file);
+    }
+
+    if (file.isStream()) {
+      return done(new PluginError('gulp-xml-edit', 'Streaming not supported'));
+    }
+
+    const content = file.contents.toString('utf-8');
+    const parser = new xml2js.Parser(settings.parserOptions);
+    const builder = new xml2js.Builder(settings.builderOptions);
+
+    parser.parseString(content, function(err, data) {
+      let content = transform.call(null, data, file);
+
+      if (!isObject(content)) {
+        done(new PluginError('gulp-xml-edit', 'transformation does not returns an object'));
+        return;
+      }
+
+      content = builder.buildObject(content);
+      file.contents = new Buffer(content);
+
+      return done(null, file);
+    });
+  };
+
+  return stream;
+};
+
+module.exports = xmlEdit;
---
+++ src/gulp-edit-xml.test.js
@@ -0,0 +1,107 @@
+const xmlEdit = require('./gulp-edit-xml');
+const es = require('event-stream');
+const Vinyl = require('vinyl');
+
+describe('gulp-xml-edit', () => {
+  it('should work in buffer mode', (done) => {
+    const stream = xmlEdit();
+    const fakeBuffer = new Buffer('<svg/>');
+    const fakeFile = new Vinyl({ contents: fakeBuffer });
+
+    stream.on('data', (file) => expect(file.contents.toString()).toEqual(fakeBuffer.toString()));
+    stream.on('end', () => done());
+    stream.write(fakeFile);
+    stream.end();
+  });
+
+  it('should let null files pass through', (done) => {
+    const stream = xmlEdit();
+    const fakeFile = new Vinyl({ path: 'null.md', contents: null });
+    let n = 0;
+
+    stream.pipe(
+      es.through(
+        (file) => {
+          expect(file.path).toBe('null.md');
+          expect(file.contents).toBe(null);
+          n++;
+        },
+        () => {
+          expect(n).toBe(1);
+          done();
+        }
+      )
+    );
+
+    stream.write(fakeFile);
+    stream.end();
+  });
+
+  it('should transform as expected', (done) => {
+    const stream = xmlEdit((data) => {
+      delete data.svg.g[0].circle[0].$.transform;
+      return data;
+    });
+    const fakeBuffer = new Buffer(
+      "<svg><g><circle cx='20' cy='20' cr='20' transform='translate(20 20)'/></g></svg>"
+    );
+    const fakeFile = new Vinyl({
+      contents: fakeBuffer
+    });
+    let n = 0;
+
+    stream.pipe(
+      es.through(
+        (file) => {
+          expect(file.contents.toString()).toBe(
+            '<svg><g><circle cx="20" cy="20" cr="20"/></g></svg>'
+          );
+          n++;
+        },
+        () => {
+          expect(n).toBe(1);
+          done();
+        }
+      )
+    );
+
+    stream.write(fakeFile);
+    stream.end();
+  });
+
+  it('should pass the raw file to the transform', (done) => {
+    let captured = null;
+    const stream = xmlEdit((data, file) => {
+      delete data.svg.g[0].circle[0].$.transform;
+      captured = file.name;
+      return data;
+    });
+    const fakeBuffer = new Buffer(
+      "<svg><g><circle cx='20' cy='20' cr='20' transform='translate(20 20)'/></g></svg>"
+    );
+    const fakeFile = new Vinyl({
+      contents: fakeBuffer,
+      name: 'expected.xml'
+    });
+    let n = 0; // eslint-disable-line no-unused-vars
+
+    stream.pipe(
+      es.through(
+        (file) => {
+          expect(file.contents.toString()).toBe(
+            '<svg><g><circle cx="20" cy="20" cr="20"/></g></svg>'
+          );
+          n++;
+        },
+        () => {
+          expect(captured).not.toBe(null);
+          expect(captured).toBe('expected.xml');
+          done();
+        }
+      )
+    );
+
+    stream.write(fakeFile);
+    stream.end();
+  });
+});
