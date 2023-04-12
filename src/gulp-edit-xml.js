af91541cad2972b7bccba9859af73a9be8c5dee1
Vivek Kumar Bansal <vkb0310@gmail.com>
2023-04-12 07:35:39 +0530

    chore: update prettier rules

--- src/gulp-edit-xml.js
+++ src/gulp-edit-xml.js
@@ -5,56 +5,61 @@ const isObject = require('lodash.isobject');
 const assign = require('lodash.defaultsdeep');
 const PluginError = require('plugin-error');
 
-const xmlEdit = function(transform, options) {
-  if (!isFunction(transform)) {
-    transform = function(data) {
-      return data;
-    };
-  }
+const xmlEdit = function (transform, options) {
+	if (!isFunction(transform)) {
+		transform = function (data) {
+			return data;
+		};
+	}
 
-  const defaults = {
-    parserOptions: {},
-    builderOptions: {
-      headless: true,
-      renderOpts: {
-        pretty: false
-      }
-    }
-  };
+	const defaults = {
+		parserOptions: {},
+		builderOptions: {
+			headless: true,
+			renderOpts: {
+				pretty: false
+			}
+		}
+	};
 
-  const settings = assign(options, defaults);
+	const settings = assign(options, defaults);
 
-  const stream = new Stream.Transform({ objectMode: true });
+	const stream = new Stream.Transform({ objectMode: true });
 
-  stream._transform = function(file, unused, done) {
-    if (file.isNull()) {
-      return done(null, file);
-    }
+	stream._transform = function (file, unused, done) {
+		if (file.isNull()) {
+			return done(null, file);
+		}
 
-    if (file.isStream()) {
-      return done(new PluginError('gulp-xml-edit', 'Streaming not supported'));
-    }
+		if (file.isStream()) {
+			return done(new PluginError('gulp-xml-edit', 'Streaming not supported'));
+		}
 
-    const content = file.contents.toString('utf-8');
-    const parser = new xml2js.Parser(settings.parserOptions);
-    const builder = new xml2js.Builder(settings.builderOptions);
+		const content = file.contents.toString('utf-8');
+		const parser = new xml2js.Parser(settings.parserOptions);
+		const builder = new xml2js.Builder(settings.builderOptions);
 
-    parser.parseString(content, function(err, data) {
-      let content = transform.call(null, data, file);
+		parser.parseString(content, function (err, data) {
+			let content = transform.call(null, data, file);
 
-      if (!isObject(content)) {
-        done(new PluginError('gulp-xml-edit', 'transformation does not returns an object'));
-        return;
-      }
+			if (!isObject(content)) {
+				done(
+					new PluginError(
+						'gulp-xml-edit',
+						'transformation does not returns an object'
+					)
+				);
+				return;
+			}
 
-      content = builder.buildObject(content);
-      file.contents = new Buffer(content);
+			content = builder.buildObject(content);
+			file.contents = new Buffer(content);
 
-      return done(null, file);
-    });
-  };
+			return done(null, file);
+		});
+	};
 
-  return stream;
+	return stream;
 };
 
 module.exports = xmlEdit;
